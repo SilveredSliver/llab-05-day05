123cded519694230c771bab94c738998b6492f39
AaronBruce <aaron.bruce@comcast.net>
2017-06-12 07:02:51 -0700

    updated branch master

--- test.js
+++ test.js
@@ -3,13 +3,19 @@
 42 lines (36 sloc)  1.41 KB
 'use strict';
 
-function testSum() { //eslint-disable-line
-  if (sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
-    console.log('%c TEST FOR sum() PASSES', 'color: green');
-  } else {
-    console.log('%c TEST FOR sum() FAILS', 'color: red');
-  }
+function sum (x,y) {
+  return [x + y, 'The sum of 4 and 7 is 11.'];
 }
+
+function testSum() { //eslint-disable-line
+if (sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
+console.log('%c TEST FOR sum() PASSES', 'color: green');
+} else {
+console.log('%c TEST FOR sum() FAILS', 'color: red');
+}
+}
+
+testSum();
 
 function testMultiply() { //eslint-disable-line
   if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
